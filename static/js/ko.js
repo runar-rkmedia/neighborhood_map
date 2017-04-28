cd87606afa83d3763b95196fdd5c0151a34d0bf3
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-04-29 01:30:31 +0200

    filter

--- static/js/ko.js
+++ static/js/ko.js
@@ -7,10 +7,33 @@ function SidepanelView() {
     self.currentPlaceData = ko.observable();
     self.showPlacesList = ko.observable();
     self.showMarkersList = ko.observable(true);
-    self.places = ko.observableArray();
+    self.placesData = ko.observableArray();
+    self.markersData = ko.observableArray();
     self.markers = ko.observableArray();
+    self.userFilter = ko.observable();
+
+    // Filter places on search
+    self.filterPlaces = ko.computed(function () {
+        if (!self.userFilter()) {
+            return self.placesData();
+        } else {
+            return ko.utils.arrayFilter(self.placesData(), function (place) {
+                return place.name.toLowerCase().indexOf(self.userFilter().toLowerCase()) !=-1;
+            });
+        }
+    });
+    // Filter markers on search
+    self.filterMarkers = ko.computed(function () {
+        if (!self.userFilter()) {
+            return self.markersData();
+        } else {
+            return ko.utils.arrayFilter(self.markersData(), function (marker) {
+                return marker.name.toLowerCase().indexOf(self.userFilter().toLowerCase()) !=-1;
+            });
+        }
+    });
     $.get("/json/places/", function(data) {
-        self.places(data.places);
+        self.placesData(data.places);
         self.showPlacesList(true);
         if (!self.currentPlaceData()) {
             self.currentPlaceData(data.places[0]);
@@ -29,12 +52,12 @@ function SidepanelView() {
     });
     // Place markers on map whenever markers changes
     self.putmarkers = ko.computed(function() {
-        if (self.google() && self.markers().length > 0) {
-            console.log(self.markers().length);
-            for (var i = 0; i < self.markers().length; i++) {
+        // hideListings();
+        if (self.google() && self.filterMarkers().length > 0) {
+            for (var i = 0; i < self.filterMarkers().length; i++) {
+                var this_marker = self.filterMarkers()[i];
                 var largeInfowindow = new google.maps.InfoWindow();
-                var this_marker = self.markers()[i];
-                console.log(this_marker.name);
+                console.log(self.filterMarkers());
                 var marker = new google.maps.Marker({
                     position: {
                         lat: this_marker.latitude,
@@ -44,6 +67,7 @@ function SidepanelView() {
                     title: this_marker.name,
                     description: this_marker.description,
                     animation: google.maps.Animation.DROP,
+                    id: this_marker.id
                 });
 
                 // Push the marker to our array of markers.
@@ -65,10 +89,9 @@ function SidepanelView() {
     };
     // Retrieve tha markers whenever the currentPlaceData changes. (caching?)
     self.retrieveMarkers = ko.computed(function() {
-        hideListings();
         if (self.currentPlaceData()) {
             $.get("/json/places/" + self.currentPlaceData().id, function(data) {
-                self.markers(data.markers);
+                self.markersData(data.markers);
                 // console.log(data);
             });
         }
