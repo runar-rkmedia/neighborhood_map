672e558e027606cde76b3d02e3824a67fc669f9c
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-04-28 22:48:02 +0200

    Change places on click

--- static/js/ko.js
+++ static/js/ko.js
@@ -5,16 +5,24 @@ function SidepanelView() {
     self.google = ko.observable(!!window.google);
     self.chosenPlaceId = ko.observable();
     self.chosenPlaceData = ko.observable();
-    $.get("/json/places/",self.chosenPlaceData);
+    self.places = ko.observableArray();
+    $.get("/json/places/",function (data) {
+        self.places(data.places);
+        if (!self.chosenPlaceData()) {
+            self.chosenPlaceData(data.places[1]);
+        }
+    });
 
     self.centerMap = ko.computed(function () {
         if (self.google() && self.chosenPlaceData()) {
-            console.log('loaded ');
-            var place = self.chosenPlaceData().places[0];
+            var place = self.chosenPlaceData();
             map.setCenter({lat: place.latitude, lng: place.longitude});
             map.setZoom(place.zoom);
         }
     });
+    self.changePlace = function (place) {
+        self.chosenPlaceData(place);
+    };
 }
 
 ko.applyBindings(new SidepanelView());
