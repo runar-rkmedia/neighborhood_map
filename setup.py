d8589a6d1b2a3b9f2103ceba836d5d26bfd2b123
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-05-01 21:02:36 +0200

    Fix overflowing Restaurants.

--- setup.py
+++ setup.py
@@ -22,33 +22,72 @@ places = [
         'description': 'Southern norway is the place to be.',
         'latitude': 52.341810,
         'longitude': 5.636922,
-        'zoom': 13,
+        'zoom': 12,
         'id': 3
     }
 ]
 markers = [
     {
         'name': 'Colloseum',
-        'description': 'Fighting for eons',
+        'description': 'Legendary gladiator-fights',
         'latitude': 41.890695,
         'longitude': 12.492051,
-        'id': 1,
         'place_id': 1
     },
     {
-        'name': 'test',
-        'description': 'test',
-        'latitude': 41.900695,
-        'longitude': 12.472051,
-        'id': 2,
-        'place_id': 1
-    },
-    {
-        'name': 'test2',
-        'description': 'test2',
-        'latitude': 41.920695,
-        'longitude': 12.472051,
-        'id': 3,
+        'name': 'Sør Arena',
+        'description': "Soccer field for local team 'Start'",
+        'latitude': 58.153613,
+        'longitude': 8.028809,
         'place_id': 2
+    },
+    {
+        'name': 'Cafè Canon',
+        'description': "Get coffee at the canon",
+        'latitude': 58.130995,
+        'longitude': 8.009542,
+        'place_id': 2
+    },
+    {
+        'name': 'Ravnedalen',
+        'description': "Park with live music, theatre and its very own restaurant.",
+        'latitude': 58.156548,
+        'longitude': 7.974629,
+        'place_id': 2
+    },
+    {
+        'name': 'Knardijk',
+        'description': "A nice bridge",
+        'latitude': 52.361235,
+        'longitude': 5.617114,
+        'place_id': 3
+    },
+    {
+        'name': 'Zeewolde Centrum',
+        'description': "A nice place",
+        'latitude': 52.330724,
+        'longitude': 5.543065,
+        'place_id': 3
+    },
+    {
+        'name': 'Leuvenumsche Beek',
+        'description': "A nice park",
+        'latitude': 52.316971,
+        'longitude': 5.708328,
+        'place_id': 3
+    },
+    {
+        'name': 'Piazza Venezia',
+        'description': 'The Piazza of Venezia',
+        'latitude': 41.896262,
+        'longitude': 12.482454,
+        'place_id': 1
+    },
+    {
+        'name': 'Vatican City',
+        'description': 'Home of the Pope',
+        'latitude': 41.902523,
+        'longitude': 12.453671,
+        'place_id': 1
     }
 ]
