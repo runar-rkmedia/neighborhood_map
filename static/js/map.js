b9028fc3e561064e12ceeacdff2152d906e90f7a
Runar Trollet Kristoffersen <runartrollet@me.com>
2017-05-01 15:46:18 +0200

    fix font-issue, delete markers when new data enters

--- static/js/map.js
+++ static/js/map.js
@@ -51,3 +51,18 @@ function populateInfoWindow(marker, infowindow) {
         infowindow.marker = null;
     }
 }
+// Sets the map on all markers in the array.
+function setMapOnAll(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setMapOnAll(null);
+}
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
